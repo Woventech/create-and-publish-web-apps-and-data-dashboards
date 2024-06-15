f0eb657dd972b0bf4ee1cfb2b6a91476357df126
Woventech <154053500+Woventech@users.noreply.github.com>
2024-06-15 21:20:39 +0000

    new commit: add st.write and pandas to show data

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+  'series_1' : [1, 2, 3, 5, 7],
+  'series_2': [10, 20, 35, 58, 100]
+}
+df = pd.DataFrame(data)
 
 st.title('Our first streamlit app ')
-st.subheader('introducin streamlit in automate everything')
+st.subheader('introducing streamlit in automate everything')
 st.write('''this is our first web app. Enjoy it!''')
+st.write(df)
